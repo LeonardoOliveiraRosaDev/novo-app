ceeca1d4a64d90099c1f32964df9f1b0f30516b6
Leonardo Oliveira Rosa <107898142+LeonardoOliveiraRosaDev@users.noreply.github.com>
2022-08-09 19:57:14 -0300

    09082022

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,18 +5,7 @@ function App() {
     // Esta é a construção da view principal do projeto novo-app
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Olá galera FullStack! Um olá mundo do ReactJS
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          
       </header>
     </div>
   );
